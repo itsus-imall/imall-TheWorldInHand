a07281056dfc30ab9eda2fb84384912da1e8d0db
Ji Su Moon <answltn5935@gmail.com>
2023-09-12 17:19:09 +0900

    feat : 손 모양 유형찾기 제작

--- src/components/HandShapeQuestion.js
+++ src/components/HandShapeQuestion.js
@@ -4,25 +4,25 @@ import * as S from '../style/questionStyled';
 const HandShapeQuestion = () => {
   const { questions } = useOutletContext();
   return (
-    <S.ModelWrapper>
-      <S.ModelImgWrapper>
-        <img
-          src={`/images/테스트페이지/260x260_01_${
-            questions[0].value.includes('갤럭시') ? '삼성' : '애플'
-          }.png`}
-          alt='model'
-        />
-        <p>1가지만 선택해 주세요</p>
-      </S.ModelImgWrapper>
-      {questions.map(({ value }) => {
+    <S.Wrapper>
+      {questions.map(({ value, summary }, index) => {
         return (
-          <S.ModelInputWrapper key={value}>
+          <S.ShapeInputWrapper key={value}>
             <input id={value} name='model' type='radio' value={value} />
-            <label htmlFor={value}>{value}</label>
-          </S.ModelInputWrapper>
+            <label htmlFor={value}>
+              <img
+                src={`/images/테스트페이지/260x260_02_손모양${index}.png`}
+                alt='손모양'
+              />
+              <div>
+                <strong>{value}</strong>
+                <span>{summary}</span>
+              </div>
+            </label>
+          </S.ShapeInputWrapper>
         );
       })}
-    </S.ModelWrapper>
+    </S.Wrapper>
   );
 };
 
--- src/style/questionStyled.js
+++ src/style/questionStyled.js
@@ -137,3 +137,36 @@ export const ModelInputWrapper = styled(InputWrapper)`
     border: 2px solid var(--input-checked-border-color);
   }
 `;
+
+export const ShapeInputWrapper = styled(InputWrapper)`
+  flex: 0 0 100%;
+  label {
+    display: flex;
+    padding: 1rem;
+    gap: 1rem;
+    img {
+      border-radius: 50%;
+      background: #d9d9d9;
+      width: 25%;
+      box-sizing: border-box;
+      padding: 0.5rem;
+    }
+    div {
+      display: flex;
+      justify-content: center;
+      align-items: flex-start;
+      flex-direction: column;
+      line-height: 1.5;
+      text-align: left;
+      word-break: keep-all;
+      strong {
+        font-size: 1rem;
+        color: #5a1de7;
+      }
+      span {
+        font-size: 0.8rem;
+        color: #7f7f7f;
+      }
+    }
+  }
+`;
